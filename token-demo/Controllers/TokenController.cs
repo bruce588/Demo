f40f0674d315701215ae188d266e9892f03c9159
Bruce.Lai <bruce588@gmail.com>
2020-06-20 21:31:13 +0800

    token-demo

---
+++ token-demo/Controllers/TokenController.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using token_demo.Helper;
+using token_demo.Models;
+
+namespace token_demo.Controllers
+{
+
+        [Authorize]
+        [Route("api/[controller]")]
+        [ApiController]
+        public class TokenController : ControllerBase
+        {
+            private JwtHelper _jwtHelper;
+
+            public TokenController(JwtHelper helper)
+            {
+                _jwtHelper = helper;
+
+            }
+            [AllowAnonymous]
+            [HttpPost("~/signin")]
+            [ProducesDefaultResponseType]
+            [ProducesResponseType(StatusCodes.Status200OK)]
+            public ActionResult<string> SignIn(LoginViewModel user)
+            {
+                if (this.ValidateUser(user))
+                {
+                // 你可以自行擴充 "roles" 加入登入者該有的角色
+                List<Claim> userClaims = new List<Claim>();
+                userClaims.Add(new Claim("roles", "Admin"));
+                userClaims.Add(new Claim("roles", "Users"));
+                return this._jwtHelper.GenerateToken(user.Username, userClaims,10);
+
+                }
+                else
+                {
+                    return this.BadRequest();
+                }
+
+
+            }
+
+            private bool ValidateUser(LoginViewModel user)
+            {
+                return true;
+            }
+
+            [HttpGet("~/claims")]
+            public IActionResult GetClaims()
+            {
+                return Ok(User.Claims.Select(p => new { p.Type, p.Value }));
+            }
+
+            [HttpGet("~/username")]
+            public IActionResult GetUserName()
+            {
+                return Ok(User.Identity.Name);
+            }
+
+            [HttpGet("~/jwtid")]
+            public IActionResult GetUniqueId()
+            {
+                var jti = User.Claims.FirstOrDefault(p => p.Type == "jti");
+                return Ok(jti.Value);
+            }
+        }
+   
+}
---
+++ token-demo/Readme.txt
@@ -0,0 +1,30 @@
+﻿step1:安裝Jwt套件
+  dotnet add package Microsoft.AspNetCore.Authentication.JwtBearer --version 3.1.5
+
+Step2:將 JwtHelper類別加到專案中
+
+Step3:Startup.ConfigureServices JwtHelper,AuthenticationScheme 注入DI容器中
+ 
+        services.AddSingleton<JwtHelper>();//身分驗證的物件註冊
+        services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+
+Step4: Startup.Configure 到Middleware中加入驗證
+        app.UseAuthentication();//(要加入驗證的Middleware)
+
+Step4:加入驗證的controller
+        TokenController.cs
+
+step5:jwt私鑰 放到 appsettings.json 檔案中
+    "JwtSettings": {
+    "Issuer": "JwtAuthDemo",
+    "SignKey": "1Zl4h9701234563IzROikK3@uK&&OEb"
+    },
+--------------------------------------------------
+step5:測試 https://localhost:5001/api/demo
+step6:Login https://localhost:5001/signin
+            帶入帳密
+            {
+            "Username":"bruce",
+            "Password":"123456"
+            }
+  
